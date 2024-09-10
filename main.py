c9834d8faea3355e1796ac6feb36b034df5cd865
Sokly Hour <hoursokly011@gmail.com>
2024-09-10 14:44:50 -0500

    add info for main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 ### INF601 - Advanced Programming in Python
 ### Sokly Hour
 ### Mini Project 1
+
+import yfinance as yf # type: ignore
+from pprint import pprint
+
+mytickers = ["MSFT", "AAPL", "", "",]
+
+msft = yf.Ticker("MSFT")
+
+# get all stock info
+# print(msft.info)
+pprint(msft.info)
+
+# get historical market data
+hist = msft.history(period="1mo")
+last_10_days = hist.tail(10)
+pprint(last_10_days)
